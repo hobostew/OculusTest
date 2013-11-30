488cf5ff37b85c82d2233d28354cfd59c98ffffd
hobostew <hobostew@gmail.com>
2013-11-30 13:22:14 -0800

    Today we kill, tomorrow we die

---
+++ Assets/Main/Scripts/Navigate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigate : MonoBehaviour
+{
+	bool isJumping = false;
+	int jumpSpeed = 15;
+	int numBumps = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+print ("update");
+		if ( isJumping )
+		{
+			print ("jumping");
+			transform.Translate(Vector3.up * jumpSpeed * Time.deltaTime, Space.World);
+			if ( ++numBumps > 50 ) isJumping = false;
+		}
+		else if ( Input.GetButtonDown( "Jump" ) )
+		{
+			print ("jump!");
+			isJumping = true;
+			numBumps = 0;
+		}
+	}
+}
